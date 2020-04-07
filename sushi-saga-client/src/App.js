10dd8163c2b47242b61fdf2522b7d70da78118ef
simon <sjtucker28@gmail.com>
2020-04-07 17:00:05 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+
+
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      sushis: [],
+      eatenSushi: [],
+      display: 0,
+      money: 150
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(sushis => { this.setState({sushis})
+    })
+  }
+
+  fourSushi = () => {
+    return this.state.sushis.slice(this.state.display, this.state.display + 4)
+  }
+
+  moreSushi = () => {
+    let newSushi = this.state.display + 4
+    this.setState({
+      display: newSushi 
+    })
+  }
+
+  eatSushi = (id, price) => {
+    if (!this.state.eatenSushi.includes(id) && this.state.money >= price) {
+      this.setState({
+        eatenSushi: [...this.state.eatenSushi, id],
+        money: this.state.money - price
+      })
+    }
+    
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+        sushis={this.fourSushi()}
+        moreSushi = {this.moreSushi}
+        eatSushi={this.eatSushi}
+        eatenSushi={this.state.eatenSushi}
+        />
+        <Table eatenSushi={this.state.eatenSushi} money={this.state.money}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,30 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  const renderSushis = () => {
+   return props.sushis.map(sushi => {
+    return <Sushi 
+    name={sushi.name} 
+    img_url={sushi.img_url} 
+    price={sushi.price} 
+    id={sushi.id} 
+    key={sushi.id}
+    eatSushi={props.eatSushi}
+    eatenSushi={props.eatenSushi}
+   />})
+  //  can use spread operator / return props.sushis.map(sushi => <Sushi {...sushi} />
+  }
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        {// Render Sushi components here!
+          renderSushis()
         }
-        <MoreButton />
+        <MoreButton moreSushi={props.moreSushi}/>
       </div>
     </Fragment>
   )
